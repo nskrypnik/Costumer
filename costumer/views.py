de2c8e63b3927c1e5515d6447fd88ed32c5ba338
Niko Skrypnik <nskrypnik@gmail.com>
2012-04-21 12:20:13 +0300

    make base.html

--- costumer/views.py
+++ costumer/views.py
@@ -1,5 +1,11 @@
 from pyramid.view import view_config
+from pyramid.httpexceptions import HTTPNotFound
+from pyramid.renderers import render
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='index.html')
 def my_view(request):
     return {'project':'costumer'}
+
+@view_config(context='pyramid.httpexceptions.HTTPNotFound')
+def html_file_lookup(request):
+    return HTTPNotFound()
